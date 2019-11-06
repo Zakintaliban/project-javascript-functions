cc40c7fb6f2363fbd81241b4c5e5555e60a40e8f
zakintaliban <nukmans23@gmail.com>
2019-11-06 13:22:42 +0700

    v2

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ function logName(name) {
   console.log(`Hello, ${name}!`);
 }
 
-const nameA = "Alpha";
-const nameB = "Betty";
+const nameA = "Alfa";
+const nameB = "Beta";
+const nameC = "Charlie";
 
 logName(nameA);
 logName(nameB);
+logName(nameC);
 
 // Feel free to have some experiments with functions
 
@@ -20,8 +22,8 @@ const checkAge = function(age) {
   }
 };
 
-checkAge(10); // You are still young
-checkAge(25); // You are old enough
+checkAge(12); // You are still young
+checkAge(18); // You are old enough
 checkAge(-2); // You are not born yet
 
 // Feel free to have some experiments with function declarations
@@ -36,10 +38,12 @@ const addMarks = (text, mark, times) => {
   console.log(newText);
 };
 
-addMarks("Hello", "!", 1); // Hello!
-addMarks("Hello", "!", 3); // Hello!!!
-addMarks("How are you", "?", 1); // How are you?
-addMarks("How are you", "?", 2); // How are you??
+addMarks("Hello", "!", 0); // Hello!
+addMarks("HELLO", "!", 4); // Hello!!!!!
+addMarks("How are you", "?", 0); // How are you?
+addMarks("HOW ARE YOU", "?", 4); // How are you?????
+addMarks("nih gak ada orangnya?  seriusan", "?", 0); // nih gak ada orangnya? seriusan?
+addMarks("NIH GAK ADA ORANGNYA?  SERIUSAN", "?", 4); // nih gak ada orangnya? seriusan?????
 
 // Feel free to have some experiments with arrow functions
 
@@ -49,12 +53,12 @@ const showNameWithAge = (name = "Unknown", age = 0) => {
   return nameWithAge; // return the value to be used later
 };
 
-const alpha = showNameWithAge("Alpha", 11);
-const betty = showNameWithAge("Betty", 42);
-const gamma = showNameWithAge("Gamma", 30);
+const alpha = showNameWithAge("Alfa", 12);
+const betty = showNameWithAge("Beta", 18);
+const gamma = showNameWithAge("Charlie", 30);
 
-console.log(alpha);
-console.log(betty);
-console.log(gamma);
+console.log(Alfa);
+console.log(Beta);
+console.log(Charlie);
 
 // Feel free to have some experiments with default parameters
